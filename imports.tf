f9934c685fb6cbf9a2d304d834425f850dee8ab0
Austin Pack <austin.sixpack@gmail.com>
2024-11-22 12:29:29 -0700

    First commit

---
+++ imports.tf
@@ -0,0 +1,49 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+/*
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-01f02c63eda68e5e1" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-0dd89288d60c2b7a2" #PublicSubnet1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-09102e3006c032d35" #PublicSubnet2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-03cbfb8dfcdda19bf" #InternetGateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-006d09c2add65f646_0.0.0.0/0" #DefaultPublicRoute
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-006d09c2add65f646" #PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-0dd89288d60c2b7a2/rtb-006d09c2add65f646" #PublicSubnet1/PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-09102e3006c032d35/rtb-006d09c2add65f646" #PublicSubnet2/PublicRouteTable
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-066787760f66837c9" #NoIngressSecurityGroup
+}
+*/
